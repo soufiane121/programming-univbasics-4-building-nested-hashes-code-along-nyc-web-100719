3cdc7c97e35a23cab253b6dce62b37e386b0f3af
soufiane oucherrou <soufiane.oucherrou@gmail.com>
2019-09-19 17:49:13 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -8,7 +8,9 @@ def second_challenge
           :name => "Lady Montague",
           :age => "54"},
        :hero => {
-          :name=> "Romeo"
+          :name=> "Romeo",
+          :age => "15",
+          :statu
             
           }
       }, 
