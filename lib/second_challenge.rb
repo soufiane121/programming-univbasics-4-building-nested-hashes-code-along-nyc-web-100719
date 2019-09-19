df080505d48715902e6ab83825aee49efa797446
soufiane oucherrou <soufiane.oucherrou@gmail.com>
2019-09-19 17:48:29 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,13 +1,15 @@
 def second_challenge
   epic_tragedy = {
     :montague => {
-      :patriache =>{
-        :name = "Lord Montague",
-        :age => "53"},
-        :matriach => {
+       :patriache =>{
+          :name = "Lord Montague",
+          :age => "53"},
+       :matriach => {
           :name => "Lady Montague",
-          :age => ""
-        }
+          :age => "54"},
+       :hero => {
+            
+          }
       }, 
     :capulet => {
       #your key/value pairs here!
