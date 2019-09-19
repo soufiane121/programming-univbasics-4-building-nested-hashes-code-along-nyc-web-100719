ef8ccbf9091b6680f60ceeedf8e8b4388b589a9a
soufiane oucherrou <soufiane.oucherrou@gmail.com>
2019-09-19 18:16:00 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,53 +1,51 @@
 def second_challenge
-  epic_tragedy = {
-    :montague => {
-       :patriache =>{
-          :name => "Lord Montague",
-          :age => "53"},
-       :matriach => {
-          :name => "Lady Montague",
-          :age => "54"},
-       :hero => {
-          :name=> "Romeo",
-          :age => "15",
-          :status =>"Alive"},
-        :hero_friends => {
-          :friend1 =>[{
-          :name => "Benvolio",
-          :age=> "17",
-          :attitude=> "worried"}],
-          :friend2 => [{
-            :name=> "Mercutio",
-            :age => "18",
-            :attitude => "hot-headed"
-          }]
-        }  
-      }, 
-    :capulet => {
-       :patriache => {
-          :name => "Lord Capulet",
-          :age => "50"},
-        :matriach =>{
-          :name => "Lady Capulet",
-          :age => "51"},
-        :heroine => {
-          :name => "Julliet",
-          :age => "15",
-          :status => "Alive"},
-        :heroine_friends =>{
-          :friend1 =>[{
-            :name=> "Steven",
-            :age => "30"
-            :attitude => "confused"}],
-          :friend2 =>[{
-            :name=> "Nurse",
-            :age=> "44",
-            :attitude =>"worried"
-          }]  
-        }  
+    epic_tragedy = {
+      :montague => {
+         :patriache =>{
+            :name => "Lord Montague",
+            :age => "53"},
+         :matriach => {
+            :name => "Lady Montague",
+            :age => "54"},
+         :hero => {
+            :name=> "Romeo",
+            :age => "15",
+            :status =>"Alive"},
+          :hero_friends => {
+            :friend1 =>[{
+            :name => "Benvolio",
+            :age=> "17",
+            :attitude=> "worried"}],
+            :friend2 => [{
+              :name=> "Mercutio",
+              :age => "18",
+              :attitude => "hot-headed"
+            }]
+          }  
+        }, 
+      :capulet => {
+         :patriache => {
+            :name => "Lord Capulet",
+            :age => "50"},
+          :matriach =>{
+            :name => "Lady Capulet",
+            :age => "51"},
+          :heroine => {
+            :name => "Julliet",
+            :age => "15",
+            :status => "Alive"},
+          :heroine_friends =>{
+            :friend1 =>[{
+              :name=> "Steven",
+              :age => "30",
+              :attitude => "confused"}],
+            :friend2 =>[{
+              :name=> "Nurse",
+              :age=> "44",
+              :attitude =>"worried"}]  
+          }  
+      }
+      
     }
-    
-  }
-
   
-end
+  end
